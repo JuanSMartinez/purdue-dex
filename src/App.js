40f726f0834b5ff30378c6821510a6cccef49692
JuanSMartinez <mart1304@purdue.edu>
2023-03-08 10:30:10 -0500

    Added pokemon team component with basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './style/App.css'
 import PokemonList from './components/PokemonList';
 import PokemonViewer from './components/PokemonViewer';
+import PokemonTeam from './components/PokemonTeam';
 
 function App() {
   return (
     <div className="App">
       <PokemonList />
       <PokemonViewer />
+      <PokemonTeam />
     </div>
   );
 }
---
+++ src/components/PokemonTeam.jsx
@@ -0,0 +1,11 @@
+import '../style/PokemonTeam.css'
+
+function PokemonTeam() {
+    return (
+        <div className="PokemonTeam">
+            This is the pokemon team
+        </div>
+    );
+}
+
+export default PokemonTeam;
