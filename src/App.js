08d6db4cce3d187626cf6f99bc10eb833a38df7e
JuanSMartinez <mart1304@purdue.edu>
2023-03-08 10:01:43 -0500

    Added pokemon list component and changed display to grid in the container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './style/App.css'
+import PokemonList from './components/PokemonList';
 
 function App() {
   return (
     <div className="App">
       <h1>Start Here</h1>
+      <PokemonList />
     </div>
   );
 }
