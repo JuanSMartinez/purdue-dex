b34da0d7868aa5b75795f259231c01e3016e8bc4
JuanSMartinez <mart1304@purdue.edu>
2023-03-08 10:02:15 -0500

    Added files for pokemon list

---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,11 @@
+import "../style/PokemonList.css"
+
+function PokemonList(){
+    return (
+        <div className="PokemonList">
+            This is the pokemon list
+        </div>
+    );
+}
+
+export default PokemonList;
