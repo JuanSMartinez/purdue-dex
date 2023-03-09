1a91c01a0c6e578aeb5b9051d806615b454e2204
JuanSMartinez <mart1304@purdue.edu>
2023-03-09 18:51:47 -0500

    Added state of focused pokemon data to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './style/App.css'
+import { useState } from 'react';
 import PokemonList from './components/PokemonList';
 import PokemonViewer from './components/PokemonViewer';
 import PokemonTeam from './components/PokemonTeam';
 
 function App() {
+  const [focusedPokemonData, setFocusedPokemonData] = useState(null);
+  
   return (
     <div className="App">
       <PokemonList />
