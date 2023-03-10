09f9932cb44c989e612ac19a680c13e34684e01e
JuanSMartinez <mart1304@purdue.edu>
2023-03-10 13:47:22 -0500

    Added types and table of base stats in viewer for clicked pokemon

--- src/components/PokemonViewer.jsx
+++ src/components/PokemonViewer.jsx
@@ -1,14 +1,77 @@
 import '../style/PokemonViewer.css'
 import loadingIcon from '../loading.png'
-import { useEffect } from 'react';
 
 function PokemonViewer({ focusedPokemon }){
-    console.log(focusedPokemon)
+    var types = [];
+    var stats = [];
+    if (focusedPokemon !== null){
+        types = focusedPokemon.types.map((typeJson, i) => {
+            return (
+                <div key={i} type={typeJson.type.name} className='type-box'>
+                    <b>{typeJson.type.name}</b>
+                </div>
+            );
+        });
+        stats = focusedPokemon.stats.map((statInfo, i) => {
+            return (
+                <tr key={i}>
+                    <td>{statInfo.stat.name}</td>
+                    <td>{statInfo.base_stat}</td>
+                </tr>
+            );
+        });
+    }
+
+    const pokemonArt = (
+        <div className='sprite'>
+            <img 
+            src={focusedPokemon ? focusedPokemon.sprites.other['official-artwork'].front_default : loadingIcon}
+            alt='Selected pokemon artwork'
+            />
+        </div>
+    );
+
+    const pokemonInfo = (
+        <div className='info'>
+            <h2>
+                <u>
+                {focusedPokemon ? `#${focusedPokemon.id} - ${focusedPokemon.name.toUpperCase()}`: 'null'}
+                </u>
+            </h2>
+            <div>
+                <img 
+                    src={focusedPokemon ? focusedPokemon.sprites.front_default : loadingIcon}
+                    alt='Selected pokemon sprite'
+                />
+                <img 
+                    src={focusedPokemon ? focusedPokemon.sprites.front_shiny : loadingIcon}
+                    alt='Selected pokemon shiny sprite'
+                />
+                <div>
+                    {types}
+                </div>
+                <div>
+                    <table id='stats-table'>
+                        <tbody>
+                            <tr>
+                                <th>Stat</th>
+                                <th>Base Value</th>
+                            </tr>
+                            {stats}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+
     return (
         <div className="PokemonViewer">
-            <div className='sprites'>
-                <img src={focusedPokemon ? focusedPokemon.sprites.other['official-artwork'].front_default : loadingIcon}/>
-                
+            {pokemonArt}
+            {pokemonInfo}
+            <div className='options'>
+                <button>Add to Team</button>
+                <button>Remove from Team</button>
             </div>
         </div>
     );
