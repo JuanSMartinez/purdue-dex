4aad7b0870457352d014bfa0639a2a26e434409d
JuanSMartinez <mart1304@purdue.edu>
2023-03-28 10:30:32 -0400

    Added information message

--- src/components/DatabaseManager.jsx
+++ src/components/DatabaseManager.jsx
@@ -6,9 +6,18 @@ function DatabaseManager(){
         <div className='DatabaseManager pokedex-panel'>
             <h2>
                 Database Management Disabled
+                <button className='pokedex-button' onClick={ShowInformation}><b>?</b></button>
             </h2>
         </div>
     );
+}
+
+function ShowInformation(){
+    const message = `
+    This panel contains Firebase database capabilities only on the main branch of the project.
+    Please checkout the main branch and run the application again if you wish to use this panel.
+    `
+    alert(message);
 }
 
 export default DatabaseManager;
